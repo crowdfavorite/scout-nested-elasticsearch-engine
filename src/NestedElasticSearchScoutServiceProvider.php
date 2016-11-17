2aee01615e40e3c847813941d786c04fc21131b8
Evan Anderson <ejdanderson@gmail.com>
2016-11-17 14:33:04 -0700

    Load ES properly

--- src/NestedElasticSearchScoutServiceProvider.php
+++ src/NestedElasticSearchScoutServiceProvider.php
@@ -4,6 +4,7 @@ namespace CF\Scout;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Scout\EngineManager;
+use Elasticsearch\ClientBuilder as Elasticsearch;
 
 
 class NestedElasticSearchScoutServiceProvider extends ServiceProvider
@@ -16,7 +17,9 @@ class NestedElasticSearchScoutServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app[EngineManager::class]->extend('nestedelasticsearch', function () {
-            return new NestedElasticSearchEngine;
-        }
+            $es = Elasticsearch::fromConfig(config('scout.elasticsearch.config'));
+
+            return new Engines\NestedElasticSearchEngine($es, config('scout.elasticsearch.index'));
+        });
     }
 }
